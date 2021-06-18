e7b1296c799bafd267db111026b3fa4543a2adac
Andrea Guffi <andreaguffi92@gmail.com>
2021-06-18 22:35:21 +0200

    implemented login

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import useLogin from "../../composables/useLogin";
+import { ref } from "vue";
+export default {
+  setup() {
+    const { error, login } = useLogin();
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("user login");
+      }
+    };
+
+    return { email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style>
+</style>
