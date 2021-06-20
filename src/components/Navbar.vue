f3656476a9c40fc6f655422fbc945ebde5b38595
Andrea Guffi <andreaguffi92@gmail.com>
2021-06-20 10:49:50 +0200

    login signup logout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <h1>
+        <router-link :to="{ name: 'Home' }">Music Playlist</router-link>
+      </h1>
+      <div class="links">
+        <button @click="handleClick">Logout</button>
+        <router-link :to="{ name: 'Signup' }" class="btn">Sign up</router-link>
+        <router-link :to="{ name: 'Login' }" class="btn">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import useLogout from "../composables/useLogout";
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const { logout } = useLogout();
+    const router = useRouter();
+
+    const handleClick = async () => {
+      await logout();
+      console.log("user logged out");
+      router.push({ name: "Login" });
+    };
+    return { handleClick };
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+nav h1 {
+  margin-left: 20px;
+}
+
+nav .links {
+  margin-left: auto;
+}
+
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+</style>
--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,32 +1,34 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <h3>Login</h3>
+    <h3>Sign up</h3>
+    <input type="text" placeholder="Display name" v-model="displayName" />
     <input type="email" placeholder="Email" v-model="email" />
     <input type="password" placeholder="Password" v-model="password" />
     <div v-if="error" class="error">{{ error }}</div>
-    <button v-if="!isPending">Login</button>
+    <button v-if="!isPending">Sign up</button>
     <button v-if="isPending" disabled>Loading</button>
   </form>
 </template>
 
 <script>
-import useLogin from "../../composables/useLogin";
+import useSignup from "../../composables/useSignup";
 import { ref } from "vue";
 export default {
   setup() {
-    const { error, login, isPending } = useLogin();
+    const { error, signup, isPending } = useSignup();
 
     const email = ref("");
     const password = ref("");
+    const displayName = ref("");
 
     const handleSubmit = async () => {
-      const res = await login(email.value, password.value);
+      const res = await signup(email.value, password.value, displayName.value);
       if (!error.value) {
-        console.log("user login");
+        console.log("user sign up");
       }
     };
 
-    return { email, password, handleSubmit, error, isPending };
+    return { email, password, displayName, isPending, error, handleSubmit };
   },
 };
 </script>
