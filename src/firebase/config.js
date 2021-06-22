039631d65df5b5f31205e0eb5650bef9ed6c5628
Andrea Guffi <andreaguffi92@gmail.com>
2021-06-22 23:18:45 +0200

    uploading img

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBbB4pwGvH4Q9CqU9d2u8P7cJdlvbvrbzI",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export {projectFirestore, projectAuth, timestamp}
+export {projectFirestore, projectAuth, projectStorage, timestamp, }
--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -1,51 +1,111 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <h4>Create New Playlist</h4>
-    <input type="text" required placeholder="Playlist title" v-model="title" />
-    <textarea
-      required
-      placeholder="Placeholder description"
-      v-model="description"
-    ></textarea>
-    <!-- upload playlist image -->
-    <label>Upload playlist cover image</label>
-    <input type="file" />
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <h4>Create a New Playlist</h4>
+      <input
+        type="text"
+        required
+        placeholder="Playlist title"
+        v-model="title"
+      />
+      <textarea
+        required
+        placeholder="Playlist description..."
+        v-model="description"
+      ></textarea>
+      <!-- upload playlist image -->
+      <label>Upload Playlist Cover Image</label>
+      <input type="file" @change="handleChange" />
+      <div class="error">{{ fileError }}</div>
 
-    <!-- <div class="error">{{ error }}</div> -->
-    <button>Create</button>
-  </form>
+      <button v-if="!isPending">Create</button>
+      <button v-else disabled>Saving...</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import useStorage from "@/composables/useStorage";
+import useCollection from "@/composables/useCollection";
+import getUser from "@/composables/getUser";
+import { timestamp } from "@/firebase/config";
 
 export default {
   setup() {
+    const { filePath, url, uploadImage } = useStorage();
+    const { error, addDoc } = useCollection("playlists");
+    const { user } = getUser();
     const title = ref("");
     const description = ref("");
 
-    const handleSubmit = () => {
-      console.log(title.value, description.value);
+    const file = ref(null);
+    const fileError = ref(null);
+    const isPending = ref(false);
+
+    const handleSubmit = async () => {
+      if (file.value) {
+        isPending.value = true;
+        await uploadImage(file.value);
+        await addDoc({
+          title: title.value,
+          description: description.value,
+          userId: user.value.uid,
+          userName: user.value.displayName,
+          coverUrl: url.value,
+          filePath: filePath.value, // so we can delete it later
+          songs: [],
+          createdAt: timestamp(),
+        });
+        isPending.value = false;
+        if (!error.value) {
+          console.log("playlist added");
+        }
+      }
+    };
+    // allowed file types
+    const types = ["image/png", "image/jpeg"];
+    const handleChange = (e) => {
+      let selected = e.target.files[0];
+      console.log(selected);
+      if (selected && types.includes(selected.type)) {
+        file.value = selected;
+        fileError.value = null;
+      } else {
+        file.value = null;
+        fileError.value = "Please select an image file (png or jpg)";
+      }
     };
 
-    return { title, description, handleSubmit };
+    return {
+      title,
+      description,
+      handleSubmit,
+      fileError,
+      handleChange,
+      isPending,
+    };
   },
 };
 </script>
 
 <style>
+form {
+  background: white;
+}
 input[type="file"] {
   border: 0;
   padding: 0;
 }
-
 label {
   font-size: 12px;
   display: block;
   margin-top: 30px;
 }
-
 button {
   margin-top: 20px;
 }
 </style>
+
+
+
